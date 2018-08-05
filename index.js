7e684e2b43f0b3b7b122229d11778612ad6e8820
Saajid Khan <saajid.khan13@stjohns.edu>
2018-08-05 18:21:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var allMusicians = [];
   for (var i=0; i < musicians.length; i++) {
-    allMusicians.push(players[i] + "plays" + instruments[i]);
+    allMusicians.push(musicians[i] + "plays" + instruments[i]);
   }
   return allMusicians;
 }
