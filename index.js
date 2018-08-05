7b31784cd82711337df3a1a2dcda49628438632c
Saajid Khan <saajid.khan13@stjohns.edu>
2018-08-05 18:20:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function theBeatlesPlay
+function theBeatlesPlay(musicians, instruments) {
+  var allMusicians = [];
+  for (var i=0; i < musicians.length; i++) {
+    allMusicians.push(players[i] + "plays" + instruments[i]);
+  }
+  return allMusicians;
+}
