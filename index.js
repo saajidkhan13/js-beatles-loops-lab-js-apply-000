44f16764ca3cf7078db56f78454f39549c41902d
Saajid Khan <saajid.khan13@stjohns.edu>
2018-08-05 18:24:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var allMusicians = [];
-  for (var i=0; i < musicians.length; i++) {
-    allMusicians.push(musicians[i] + "plays" + instruments[i]);
+  var allMembers = [];
+  for (var i = 0; i < musicians.length; i++) {
+    allMembers.push(musicians[i] + "plays" + instruments[i]);
   }
-  return allMusicians;
+  return allMembers;
 }
