8f955ca500d971065b2d2557fd3c07f2bd2ef489
Saajid Khan <saajid.khan13@stjohns.edu>
2018-08-05 18:29:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,11 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(){
-  var 
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var statements = [];
+  var i = 0
+  while ( i < facts.length) {
+    statements.push(facts[i]) + " !!! ") i++; }
+
+  return statements; 
 }
